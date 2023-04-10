1445e977571795ce9cba5a88baaa5eef7b2aba2b
LuisM11 <lumarinb@unal.edu.co>
2023-04-10 00:06:15 -0500

    change URI to https protocol

--- src/api/NotesAPI.js
+++ src/api/NotesAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const defaultApi = axios.create({
-  baseURL: "http://notesappbackend-production.up.railway.app/api/v1",
+  baseURL: "https://notesappbackend-production.up.railway.app/api/v1",
 });
 
 export const sleep = ms => new Promise(resolve => setTimeout(resolve, ms*1000));
